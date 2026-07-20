83fa25a547925af7e1b84ae231fbeabc7cc2be17
Richard Yu <burritoman@gmail.com>
2026-07-20 19:55:48 -0400

    added some files for future entries. Touched up JS a bit as it was wrong syntax wise (whoops), implemented a nice profile banner at the top of the page

--- portfolio/dropdown.js
+++ portfolio/dropdown.js
@@ -1,21 +1,25 @@
 const searchBar = document.querySelector(".search-bar");
-const options = document.querySelectorAll(".search-dropdown a");
-const noresult = document.querySelector(".noresults")
+const options = document.querySelectorAll(".search-dropdown a:not(#noresults)");
+const noresult = document.querySelector("#noresults");
 
-searchBar.addEventListener("input", ()){
+noresult.hidden = true;
+
+searchBar.addEventListener("input", () => {
     const text = searchBar.value.toLowerCase();
 
-    Boolean foundoption = false;
+    let foundoption = false;
 
-    options.forEach((option){
-        option.style.display = option.textContent.includes(text) ? "block" : "none";
-        if(option.style.display == "block"){
+    options.forEach((option) => {
+        option.style.display = option.textContent.toLowerCase().includes(text) ? "block" : "none";
+        if(option.style.display === "block"){
             foundoption = true;
         }
-    )}
+    });
 
     if(!foundoption){
         noresult.hidden = false;
+    } else {
+        noresult.hidden = true;
     }
     
-)}
+});
